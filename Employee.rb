dea3229edbf479f51e1f5e28d2996a5dcf8d8609
Steph and Francis <fake.com>
2016-06-20 15:38:34 -0400

    Updated Employee and Manager class

--- Employee.rb
+++ Employee.rb
@@ -1,17 +1,27 @@
 class Employee
 
-  attr_reader :name
-  attr_accessor :title, :salary, :boss
+  attr_accessor :title, :salary, :boss, :name
 
   def initialize(name, title, salary, boss = nil)
-    @name = name,
-    @title = title,
-    @salary = 0,
-    @boss = boss 
+    @name = name
+    @title = title
+    @salary = salary
+    @boss = boss
+    add_boss(boss)
   end
 
   def bonus(multiplier)
     @salary * multiplier
   end
 
+  def add_boss(boss)
+    boss.add_employee(self) unless boss.nil?
+  end
+
 end
+
+
+# ned = Manager.new("ned", "founder", 1000000)
+# darren = Manager.new("darren", "ta manager", 78,000, ned)
+# shawna = Employee.new("shawna", "ta", 12000, darren)
+# david = Employee.new("david", "ta", 10000, darren)
--- Manager.rb
+++ Manager.rb
@@ -1,19 +1,35 @@
-require_relative "/Employee.rb"
+require_relative "Employee.rb"
 
 class Manager < Employee
 
   attr_accessor :employees
 
   def initialize(name, title, salary, boss = nil)
-    super
+
+    super(name, title, salary, boss)
     @employees = []
   end
 
   def bonus(multiplier)
     total = 0
     @employees.each do |employee|
-      total += employee.salary
+      if employee.class == Manager
+        employee.employees.each do |employee2|
+          total += employee2.salary
+        end
+        total += employee.salary
+      else
+        total += employee.salary
+      end
     end
     total * multiplier
   end
+
+
+  def add_employee(employee)
+
+    self.employees << employee
+    employee.boss = self
+  end
+
 end
